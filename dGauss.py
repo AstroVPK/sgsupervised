ea7fdbf46393a30959f63466150221c06ae626dd
Jose Garmilla <garmilla@princeton.edu>
2015-09-03 18:12:19 -0400

    Add logistic fit

--- dGauss.py
+++ dGauss.py
@@ -1,10 +1,22 @@
 from sklearn.svm import SVC, LinearSVC 
+from sklearn.linear_model import LogisticRegression 
 from sklearn.grid_search import GridSearchCV
 
 def linearFit(trainingSet, n_jobs=4, **estKargs):
     X, Y = trainingSet.getTrainSet()
     estimator = LinearSVC(**estKargs)
     param_grid = {'C':[0.1, 1.0, 10.0]}
+    clf = GridSearchCV(estimator, param_grid, n_jobs=n_jobs)
+    clf.fit(X, Y)
+    X, Y = trainingSet.getTestSet()
+    score = clf.score(X, Y)
+    print "score=", score
+    return clf
+
+def logisticFit(trainingSet, n_jobs=4, **estKargs):
+    X, Y = trainingSet.getTrainSet()
+    estimator = LogisticRegression(**estKargs)
+    param_grid = {'C':[ 1.0e5, 2.0e5, 4.0e5, 1.0e6]}
     clf = GridSearchCV(estimator, param_grid, n_jobs=n_jobs)
     clf.fit(X, Y)
     X, Y = trainingSet.getTestSet()
