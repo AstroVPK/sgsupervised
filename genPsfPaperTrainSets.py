9e8ccadad5c5d15679e3e1ee752da9257827005e
Jose Garmilla <garmilla@princeton.edu>
2016-01-04 14:38:18 -0500

    Update genTrainSet script

--- genPsfPaperTrainSets.py
+++ genPsfPaperTrainSets.py
@@ -2,12 +2,16 @@ import pickle
 
 import supervisedEtl as etl
 
+concatBands=False
 bands = ['g', 'r', 'i', 'z', 'y']
 
 cat = etl.afwTable.SimpleCatalog.readFits('/scr/depot0/garmilla/HSC/matchDeepCoaddMeas-137520151126CosmosGRIZY.fits')
 
-trainSet = etl.extractTrainSet(cat, inputs=['snrPsf', 'mag', 'ext', 'extHsmDeconv', 'seeing', 'dGaussRadInner', 'dGaussRadRat', 'dGaussAmpRat'], bands=bands, concatBands=False)
-with open('trainSetGRIZY.pkl', 'wb') as f: pickle.dump(trainSet, f)
+trainSet = etl.extractTrainSet(cat, inputs=['snrPsf', 'mag', 'ext', 'extHsmDeconv', 'seeing', 'dGaussRadInner', 'dGaussRadRat', 'dGaussAmpRat'], bands=bands, concatBands=concatBands)
+if concatBands:
+    with open('trainSetConcatGRIZY.pkl', 'wb') as f: pickle.dump(trainSet, f)
+else:
+    with open('trainSetGRIZY.pkl', 'wb') as f: pickle.dump(trainSet, f)
 
 for band in bands:
     trainSet = etl.extractTrainSet(cat, inputs=['snrPsf', 'mag', 'ext', 'extHsmDeconv', 'seeing', 'dGaussRadInner', 'dGaussRadRat', 'dGaussAmpRat'], bands=[band])
