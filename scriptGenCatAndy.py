1ae3b5fe5d14cec55af6f0e0ca0174e94668be96
garmilla <garmilla@astro.princeton.edu>
2016-02-18 12:54:07 -0500

    Add magnitude to ucd list

--- scriptGenCatAndy.py
+++ scriptGenCatAndy.py
@@ -12,7 +12,7 @@ def getGood(mags, magMin, magMax):
 with open('trainSet.pkl', 'rb') as f: trainSet = pickle.load(f)
 
 X, XErr, Y = trainSet.getAllSet(standardized=False)
-mags = trainSet.getAllMags(); exts = trainSet.getAllExts()
+mags = trainSet.getAllMags()[:,2]; exts = trainSet.getAllExts()
 ids = trainSet.getAllIds(); ras = trainSet.getAllRas(); decs = trainSet.getAllDecs()
 
 good = np.logical_and(True, exts < 0.4)
@@ -46,8 +46,8 @@ good = np.logical_and(True, pStar >= 0.0)
 ids = ids[good]; ras = ras[good]; decs = decs[good]
 exts = exts[good]; pStar = pStar[good]
 
-arr = np.zeros((len(ids),), dtype=('i8, f4, f4, f4, f4'))
+arr = np.zeros((len(ids),), dtype=('i8, f4, f4, f4, f4, f5'))
 arr['f0'] = ids[:,2]; arr['f1'] = ras[:,2]; arr['f2'] = decs[:,2]
-arr['f3'] = exts; arr['f4'] = pStar
-header = 'id, RA (rads), Dec (rads), extendedness (mag_psf-mag_model), probability of being a star based on colors only'
-np.savetxt('ucd.dat', arr, fmt='%d, %.18e, %.18e, %.18e, %.18e', header=header)
+arr['f3'] = mags; arr['f4'] = exts; arr['f5'] = pStar
+header = 'id, RA (rads), Dec (rads), magnitude HSC-I, extendedness (mag_psf-mag_model), probability of being a star based on colors only'
+np.savetxt('ucd.dat', arr, fmt='%d, %.18e, %.18e, %.18e, %.18e, %.18e', header=header)
