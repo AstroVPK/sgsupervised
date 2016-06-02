a49caf7d1d533fecfab6ec92e4680acfdc617f90
garmilla <garmilla@astro.princeton.edu>
2016-06-01 22:09:55 -0400

    Save minor changes

--- plotsScript.py
+++ plotsScript.py
@@ -2075,7 +2075,7 @@ def makeCosmosWidePlots(band='i', size=100000, fontSize=18):
             tick.label.set_fontsize(fontSize)
     plt.show()
 
-def makeCosmosWideScoresPlot(fontSize=18):
+def makeCosmosWideScoresPlot(fontSize=18, cuts=[0.1, 0.5, 0.9], style = ['--', '-', ':']):
     with open('clfsColsExt.pkl', 'rb') as f:
         clfs = pickle.load(f)
     magBins = [(18.0, 22.0), (22.0, 24.0), (24.0, 25.0), (25.0, 26.0)]
@@ -2083,13 +2083,16 @@ def makeCosmosWideScoresPlot(fontSize=18):
     for i, name in enumerate(_names):
         with open('trainSet{0}.pkl'.format(name), 'rb') as f:
             trainSet = pickle.load(f)
-        X, XErr, Y = trainSet.genColExtTrainSet(mode='all')
-        mags = trainSet.getAllMags(band='i')
+        #X, XErr, Y = trainSet.genColExtTrainSet(mode='all')
+        #mags = trainSet.getAllMags(band='i')
+        X, XErr, Y = trainSet.genColExtTrainSet(mode='test')
+        mags = trainSet.getTestMags(band='i')
         clfXd = dGauss.XDClfs(clfs=clfs, magBins=magBins)
         posteriors = clfXd.predict_proba(X, XErr, mags)
         figPosts = plt.figure(figsize=(16, 6), dpi=120)
         axPostT = figPosts.add_subplot(1, 2, 1)
         axPostV = figPosts.add_subplot(1, 2, 2)
+        figPosts.suptitle(name, fontsize=fontSize)
         axPostT.set_xlabel('P(Star|Colors+Extendedness)', fontsize=fontSize)
         axPostT.set_ylabel('Star Fraction', fontsize=fontSize)
         axPostV.set_xlabel(r'$\mathrm{Mag}_{cmodel}$ HSC-I', fontsize=fontSize)
@@ -2111,6 +2114,8 @@ def makeCosmosWideScoresPlot(fontSize=18):
             else:
                 axPostV.plot(mags[idx], posteriors[idx], marker='.', markersize=2, color='red')
         axPostV.set_xlim((18.0, 25.0))
+        for i, cut in enumerate(cuts):
+            axPostV.plot([18.0, 25.0], [cut, cut], linestyle=style[i], color='black')
         for ax in figPosts.get_axes():
             for tick in ax.xaxis.get_major_ticks():
                 tick.label.set_fontsize(fontSize)
